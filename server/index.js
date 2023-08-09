abf6046b2e8b9d33477f827c36e8539da73e42d3
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-09 15:09:49 +0530

    Added database connection & error middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
+
+const dbURL = process.env.MONGO_URL;
+mongoose
+    .connect(dbURL)
+    .then(() => console.log("Connected to DB successfully!"))
+    .catch(err => console.log(err));
 
 const app = express();
 
-app.get("/api/test", (req, res) => {
-    res.json({ message: "Success" });
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+    if (!err.statusCode) {
+        return res.status(500).json({ message: err.message, statusCode: 500 });
+    }
+    const { statusCode } = err;
+    res.status(statusCode).json(err);
 });
 
 const PORT = 3000;
