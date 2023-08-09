ae545723eadb42e20c6450cd3bd57b00e32af56e
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-09 21:37:19 +0530

    Added verifyToken middleware

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,5 @@
 import ExpressError from "./utils/ExpressError.js";
+import jwt from "jsonwebtoken";
 
 export const isUserLoggedIn = (req, res, next) => {
     // If a user is already logged in
@@ -8,4 +9,18 @@ export const isUserLoggedIn = (req, res, next) => {
     }
     // Else move forward to further middlewares
     next();
+};
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+        return next(new ExpressError("Missing token!", 403));
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+        if (err) {
+            return next(new ExpressError("Invalid token!", 401));
+        }
+        req.session.userId = data.id;
+        next();
+    });
 };
