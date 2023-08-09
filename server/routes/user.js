7b6bc96ee5db59d7fddb7afbcdf05e4fb658a5e9
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-09 16:32:52 +0530

    Utilized middleware and added logout route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,11 @@ import express from "express";
 import catchAsync from "../utils/catchAsync.js";
 const router = express.Router();
 
-import { register, login } from "../controllers/users.js";
+import { register, login, logout } from "../controllers/users.js";
+import { isUserLoggedIn } from "../middleware.js";
 
-router.route("/register").post(catchAsync(register));
-router.route("/login").post(catchAsync(login));
+router.route("/register").post(isUserLoggedIn, catchAsync(register));
+router.route("/login").post(isUserLoggedIn, catchAsync(login));
+router.route("/logout").get(logout);
 
 export default router;
