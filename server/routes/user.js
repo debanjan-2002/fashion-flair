8e3841cfe126c1f30ec0363c3c57b9098f55b1d0
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-09 15:36:11 +0530

    Added login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,8 +2,9 @@ import express from "express";
 import catchAsync from "../utils/catchAsync.js";
 const router = express.Router();
 
-import { register } from "../controllers/users.js";
+import { register, login } from "../controllers/users.js";
 
 router.route("/register").post(catchAsync(register));
+router.route("/login").post(catchAsync(login));
 
 export default router;
