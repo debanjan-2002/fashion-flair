8352983b65ead9279604a119d80cc4270cd54643
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-18 00:00:14 +0530

    fetching open ai key from environment file

--- server/utils/fetchResponse.js
+++ server/utils/fetchResponse.js
@@ -8,7 +8,7 @@ import { formatData } from "./formatData.js";
 dotenv.config();
 
 const openai = new OpenAI({
-    apiKey: "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"
+    apiKey: process.env.OPENAI_API_KEY
 });
 
 let catelog = ``;
@@ -41,7 +41,6 @@ product_ids: [This will contain the ids of the products that you will recommend]
 
 export const conversationsHistory = new Map();
 
-
 export const fetchResponse = async (question, id) => {
     addNewConversationToHistory(conversationsHistory, question, id);
 
@@ -55,14 +54,14 @@ export const fetchResponse = async (question, id) => {
             model: "gpt-3.5-turbo",
             messages: message
         });
-        
+
         const answer = completion.choices[0].message;
         addNewConversationToHistory(conversationsHistory, answer, id);
 
         // console.log(JSON.parse(answer.content).response);
         return answer;
     } catch (err) {
-        console.log(err.message);
+        // console.log(err.message);
         return {
             role: "assistant",
             content:
