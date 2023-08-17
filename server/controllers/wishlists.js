b4e4b4148a59bb371c77265a817162a90925a61c
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-17 23:57:22 +0530

    Added logic for fetching and adding items to wishlist

---
+++ server/controllers/wishlists.js
@@ -0,0 +1,40 @@
+import User from "../models/users.js";
+import Product from "../models/products.js";
+
+export const getWishList = async (req, res, next) => {
+    // get the user id from session
+    const { userId } = req.session;
+
+    // get the user from database
+    const user = await User.findById(userId);
+    // events associated with the user
+    const events = user.events;
+    const productIds = [];
+
+    // extracting the product ids from all the events
+    events.forEach(event => productIds.push(event.product_id));
+
+    // finding the products using the product ids
+    const products = await Product.find({ _id: { $in: productIds } });
+    res.json({ products });
+};
+
+export const addItemToWishList = async (req, res, next) => {
+    // get the user id from session
+    const { userId } = req.session;
+    // get product id and event type from request body
+    const { productId, eventType } = req.body;
+
+    // find the user from database
+    const user = await User.findById(userId);
+
+    // push the new event details
+    user.events.push({
+        event_type: eventType,
+        product_id: productId
+    });
+    // save the updated user
+    await user.save();
+
+    res.json({ message: "Item added to wishlist successfully!" });
+};
