974f676a91ceef9a14b606673acbf3c453caf4c1
Debanjan Poddar <debanjan2602@gmail.com>
2023-08-18 20:51:01 +0530

    Added route for removing item from wishlist

--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -2,12 +2,17 @@ import express from "express";
 import catchAsync from "../utils/catchAsync.js";
 const router = express.Router();
 
-import { getWishList, addItemToWishList } from "../controllers/wishlists.js";
+import {
+    getWishList,
+    addItemToWishList,
+    deleteItemFromWishList
+} from "../controllers/wishlists.js";
 import { verifyToken } from "../middleware.js";
 
 router
     .route("/")
     .get(verifyToken, catchAsync(getWishList))
-    .post(verifyToken, catchAsync(addItemToWishList));
+    .post(verifyToken, catchAsync(addItemToWishList))
+    .delete(verifyToken, catchAsync(deleteItemFromWishList));
 
 export default router;
